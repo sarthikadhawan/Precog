c28a9fce155feb6caacb54db2b7efdc0f6d20d77
xx621998xx <sarthika15170@iiitd.ac.in>
2017-12-10 20:38:56 +0530

    Update settings.py

--- django_precog/settings.py
+++ django_precog/settings.py
@@ -17,7 +17,7 @@ from os import environ
 
 #from .base import *
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost','*']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost:8000/home','*']
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
